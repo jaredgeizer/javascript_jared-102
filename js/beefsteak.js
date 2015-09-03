8654198d2ec13b2a37bc459a37d841015ac01c93
jaredgeizer <jaredg@Jareds-MacBook-Pro.local>
2015-09-03 15:41:44 -0400

    first commit
    worked on figuring out how to use if statements

---
+++ js/beefsteak.js
@@ -0,0 +1,32 @@
+var randomNumber = 1;
+var randomNumber2 = 2;
+var randomNumber3 = 3;
+var randomNumber4 = 4;
+var randomNumber5 = 5;
+var randomNumber6 = 6;
+
+
+
+
+
+if (randomNumber > randomNumber3) {
+    console.log("true statement");
+} else if (randomNumber2 === randomNumber4) {
+    console.log("second true statement");
+} else if (randomNumber3 !== randomNumber5) {
+    console.log("third true statement");
+} else if (randomNumber4 < randomNumber6) {
+    console.log("fourth true statement");
+} else {
+    console.log("final true statement");
+}
+//right now this counts the number of characters in the variable
+ 
+
+
+
+
+
+
+ 
+    
